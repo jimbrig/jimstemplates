4c25eb1aef3c999a57880b59c873758191478d9b
Jimmy Briggs <jimbrig2011@outlook.com>
2020-12-11 13:35:40 -0500

    initial package skeleton

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# rpackage
+# jimstemplates
 
 <!-- badges: start -->
 
@@ -9,53 +9,8 @@
 WIP](https://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)
 <!-- badges: end -->
 
-The goal of rpackage is to …
-
 ## Installation
 
-You can install the released version of rpackage from
-[CRAN](https://CRAN.R-project.org) with:
-
 ``` r
-install.packages("rpackage")
+remotes::install_github("jimbrig/jimstemplates")
 ```
-
-And the development version from [GitHub](https://github.com/) with:
-
-``` r
-# install.packages("devtools")
-devtools::install_github("jimbrig/rpackage-template")
-```
-
-## Example
-
-This is a basic example which shows you how to solve a common problem:
-
-``` r
-library(rpackage)
-## basic example code
-```
-
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
-```
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date.
-
-You can also embed plots, for example:
-
-<img src="man/figures/README-pressure-1.png" width="100%" />
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub!
---
+++ tests/testthat/test-docker.R
@@ -0,0 +1,135 @@
+context("Test docker or Dockerfile related function.")
+
+pattern <- "aaa"
+
+scoped_temporary_project <- function(dir = fs::file_temp(pattern = pattern),
+                                     env = parent.frame(),
+                                     rstudio = FALSE) {
+  scoped_temporary_thing(dir, env, rstudio, "project")
+}
+
+scoped_temporary_thing <- function(dir = fs::file_temp(pattern = pattern),
+                                   env = parent.frame(),
+                                   rstudio = FALSE,
+                                   thing = c("package", "project")) {
+  thing <- match.arg(thing)
+  if (fs::dir_exists(dir)) {
+    ui_stop("Target {usethis::ui_code('dir')} {usethis::ui_path(dir)} already exists.")
+  }
+
+  old_project <- usethis:::proj$cur
+  ## Can't schedule a deferred project reset if calling this from the R
+  ## console, which is useful when developing tests
+  if (identical(env, globalenv())) {
+    usethis::ui_done("Switching to a temporary project!")
+    if (!is.null(old_project)) {
+      command <- paste0('proj_set(\"', old_project, '\")')
+      usethis::ui_todo(
+        "Restore current project with: {usethis::ui_code(command)}"
+      )
+    }
+  } else {
+    withr::defer({
+      withr::with_options(
+        list(usethis.quiet = TRUE),
+        usethis::proj_set(old_project, force = TRUE)
+      )
+      setwd(old_project)
+      fs::dir_delete(dir)
+    }, envir = env)
+  }
+
+  withr::local_options(list(usethis.quiet = TRUE))
+  switch(
+    thing,
+    package = usethis::create_package(dir, rstudio = rstudio, open = FALSE,
+                                      check_name = FALSE),
+    project = usethis::create_project(dir, rstudio = rstudio, open = FALSE)
+  )
+  usethis::proj_set(dir)
+  setwd(dir)
+  invisible(dir)
+}
+
+
+test_that("use_docker creates a Dockerfile",{
+  scoped_temporary_project()
+  use_docker()
+  expect_proj_file("Dockerfile")
+})
+
+test_that("use_docker_packages fails if there is no Dockerfile",{
+  scoped_temporary_project()
+  expect_oops(use_docker_packages("here"))
+})
+
+test_that("use_docker_packages warns about github packages",{
+  scoped_temporary_project()
+  use_docker()
+  # should warn about no github=TRUE
+  expect_warning(use_docker_packages("r-lib/usethis@b2e894e", open = FALSE),
+                 "github = TRUE")
+  # should warn about missing fixed version
+  expect_error(
+    use_docker_packages("r-lib/usethis",
+                        github = TRUE,
+                        open = FALSE),
+    "fixed version",
+    class = "usethis_error"
+  )
+  # shouldn't warn about anything
+  expect_warning(use_docker_packages("r-lib/usethis@b2e894e",
+                                     github = TRUE,
+                                     open = FALSE),
+                 NA)
+  expect_warning(use_docker_packages("usethis",
+                                     github = TRUE,
+                                     open = FALSE),
+                 NA)
+})
+
+test_that("docker_entry_install returns only characters", {
+  expect_length(docker_entry_install("test",
+                                     "installGithub.r"), 2L)
+
+  expect_length(docker_entry_install(c("test", "another_test"),
+                                     "installGithub.r"), 3L)
+
+  expect_identical(docker_entry_install("test",
+                                        "installGithub.r"),
+                   c("RUN installGithub.r \\ ", "  test"))
+
+  expect_identical(docker_entry_install(c("test", "another_test"),
+                                        "installGithub.r"),
+                   c("RUN installGithub.r \\ ", "  test \\ ", "  another_test"))
+})
+
+test_that("use_docker_packages actually adds them to the Dockerfile", {
+  scoped_temporary_package()
+  use_docker()
+  use_docker_packages(c("test1", "test2"))
+  dockerfile <- readLines("Dockerfile")
+  expect_match(dockerfile, "test1", all = FALSE)
+  expect_match(dockerfile, "test2", all = FALSE)
+})
+
+test_that("docker entry only appends stuff", {
+  scoped_temporary_package()
+  use_docker()
+  dockerfile1 <- readLines("Dockerfile")
+  docker_entry("test", write = TRUE, open = TRUE, append = TRUE)
+  dockerfile2 <- readLines("Dockerfile")
+  expect_identical(dockerfile1, dockerfile2[-length(dockerfile2)])
+  expect_identical(dockerfile2[length(dockerfile2)], "test")
+
+  dockerfile3 <- docker_entry("test", write = FALSE, open = TRUE, append = TRUE)
+  expect_identical(dockerfile3, c(dockerfile2, "test"))
+})
+
+test_that("docker_get extracts correct lines", {
+  scoped_temporary_project()
+  use_docker()
+  # cat(dockerfile, file = "Dockerfile")
+  expect_identical(docker_get_packages(),
+                   c("anytime", "dplyr", "lubridate", "readr", "usethis"))
+})
